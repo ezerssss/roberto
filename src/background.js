ac9c7206cc2f71d86f50a68a83e25dad4f8a2359
ezerssss <magbanuaezra@gmail.com>
2024-05-05 19:38:25 +0800

    daily check

--- src/background.js
+++ src/background.js
@@ -8,7 +8,7 @@ chrome.runtime.onInstalled.addListener(async ({ reason }) => {
   // Create an alarm so we have something to look at in the demo
   await chrome.alarms.create('moodle-alarm', {
     delayInMinutes: 0,
-    // periodInMinutes: 1440,
-    periodInMinutes: 0.3,
+    periodInMinutes: 1440,
+    // periodInMinutes: 0.3,
   });
 });
